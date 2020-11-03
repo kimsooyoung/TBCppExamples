623ffb92df6a1ea733728f7db965000ea3eab7ba
kimsooyoung <tge1375@naver.com>
2020-11-03 10:24:14 +0900

    Update: 2_4 까지

--- 2_2_integers/src/main.cpp
+++ 2_2_integers/src/main.cpp
@@ -5,18 +5,45 @@
 int main()
 {
     using namespace std;
+
+    {
+        short s = 1; // 2 bytes = 2 * bits = 16 bits
+        int i = 1;
+        long l = 1;
+        long long ll = 1;
+
+        std::cout << "sizeof : short" << sizeof(short) << std::endl;
+        std::cout << "sizeof : int" << sizeof(int) << std::endl;
+        std::cout << "sizeof : long" << sizeof(long) << std::endl;
+        std::cout << "sizeof : long long" << sizeof(long long) << std::endl;
+    }
+
     // overflow example
+    {
+        short val;
 
-    short val;
+        cout << pow(2, sizeof(val) * 8 - 1) - 1 << endl;
+        cout << numeric_limits<short>::max() << endl;
+        cout << numeric_limits<short>::min() << endl;
+        // min and lowest will same for integer types
+        cout << numeric_limits<short>::lowest() << endl;
 
-    cout << pow(2, sizeof(val) * 8 - 1) - 1 << endl;
-    cout << numeric_limits<short>::max() << endl;
-    cout << numeric_limits<short>::min() << endl;
-    cout << numeric_limits<short>::lowest() << endl;
+        short s = numeric_limits<short>::max(); // 32768;
+        s += 1;
 
-    short s = 32768;
+        cout << s << endl;
+    }
 
-    cout << s + 1 << endl;
+    {
+        unsigned int i = -1;
+        std::cout << i << std::endl;
+    }
+
+    {
+        int i = 22 / 4;
+        std::cout << "devide in integer " << i << std::endl;
+        std::cout << "devide in float " << (float) 22 / 4 << std::endl;
+    }
 
     return 0;
 }
---
+++ 2_3_fixed_width_integers/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+
+int main(){
+
+    std::int8_t i_8{70};
+    std::int16_t i_16{5};
+    
+    std::int_fast8_t t{65};
+    std::int_least64_t ss{3};
+
+    return 0;
+}
