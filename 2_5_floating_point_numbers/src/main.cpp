be3ea00e8b6ef4a4e51e24c2e94428767cf6d083
kimsooyoung <tge1375@naver.com>
2020-11-03 22:27:18 +0900

    Update: Chapter 5 and 6 added

--- 2_5_floating_point_numbers/src/main.cpp
+++ 2_5_floating_point_numbers/src/main.cpp
@@ -6,36 +6,68 @@
 
 int main()
 {
-    // float f(3.14f);
-    // double d;
-    // long double lg;
+    {
+        float f(3.14f); // 31.4 * 0.1
+        double d;
+        long double ld;
 
-    // std::cout << 3.14e-1 << std::endl;
-    // std::cout << 3.14e+2 << std::endl;
+        std::cout << 3.14 << std::endl;
+        std::cout << 31.4e-1 << std::endl;
+        std::cout << 314e-2 << std::endl;
+        std::cout << 31.4e1 << std::endl;
+        std::cout << 31.4e3 << std::endl;
 
-    // std::cout << std::numeric_limits<float>::lowest() << std::endl;
-    // std::cout << std::numeric_limits<double>::lowest() << std::endl;
-    // std::cout << std::numeric_limits<long double>::lowest() << std::endl;
 
-    // std::cout << std::numeric_limits<float>::max() << std::endl;
-    // std::cout << std::numeric_limits<double>::max() << std::endl;
-    // std::cout << std::numeric_limits<long double>::max() << std::endl;
+        std::cout << sizeof(f) << std::endl;
+        std::cout << sizeof(d) << std::endl;
+        std::cout << sizeof(ld) << std::endl;
 
-    // std::cout << std::numeric_limits<float>::min() << std::endl;
-    // std::cout << std::numeric_limits<double>::min() << std::endl;
-    // std::cout << std::numeric_limits<long double>::min() << std::endl;
+        std::cout << "max number of float type : " <<  std::numeric_limits<float>::max() << std::endl;
+        std::cout << "max number of double type : " <<  std::numeric_limits<double>::max() << std::endl;
+        std::cout << "max number of long double type : " <<  std::numeric_limits<long double>::max() << std::endl;
+        
+        std::cout << "lowest number of float type : " << std::numeric_limits<float>::lowest() << std::endl;
+        std::cout << "lowest number of double type : " << std::numeric_limits<double>::lowest() << std::endl;
+        std::cout << "lowest number of float type : " << std::numeric_limits<long double>::lowest() << std::endl;
 
-    std::cout << std::setprecision(5) << std::endl;
-    std::cout << 1.0 / 0.3 << std::endl;
+        // std::cout << std::numeric_limits<float>::min() << std::endl;
+        // std::cout << std::numeric_limits<double>::min() << std::endl;
+        // std::cout << std::numeric_limits<long double>::min() << std::endl;
+    }
 
-    double zero = 0.0;
-    double posinf = 5.0 / zero;
-    double neginf = -5.0 / zero;
-    double nan = zero / zero;
+    {
+        std::cout << std::setprecision(16) << std::endl;
+        std::cout <<  "1.0 / 0.3 : " << 1.0 / 0.3 << std::endl;
 
-    std::cout << posinf << " " << std::isinf(posinf) << std::endl;
-    std::cout << neginf << " " << std::isinf(neginf) << std::endl;
-    std::cout << nan << " " << std::isnan(nan) << std::endl;
+        float f(123456789.0f);
+
+        std::cout << std::setprecision(9);
+        std::cout << f << std::endl; // 123456792
+
+        double d(0.1);
+
+        std::cout << std::setprecision(17);
+        std::cout << d << std::endl; // 0.10000000000000001
+
+        double d2(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1);
+        std::cout << std::setprecision(17);
+
+        std::cout << d2 << std::endl; // 0.99999999999999989
+    }
+
+    {
+        double zero = 0.0;
+        double posinf = 5.0 / zero;
+        double neginf = -5.0 / zero;
+        double nan = zero / zero;
+
+        std::cout << posinf << " " << std::isinf(posinf) << std::endl;
+        std::cout << neginf << " " << std::isnan(neginf) << std::endl;
+        std::cout << nan << " " << std::isnan(nan) << std::endl;
+        std::cout << 1.0 << " " << std::isnan(1.0) << std::endl;
+
+    }
+
 
     return 0;
 }
--- 2_6_boolean_and_if/src/main.cpp
+++ 2_6_boolean_and_if/src/main.cpp
@@ -8,18 +8,59 @@ bool isEven(int num)
         return false;
 }
 
+bool isEqual(int a, int b){
+    return (a == b);
+}
+
 int main()
 {
     int user_val;
 
-    std::cout << "Type any Number : ";
-    std::cin >>
-        user_val;
+    {
+        bool b1(true);
+        bool b2(0);
+        bool b3(false); // copy initialization
 
-    if (isEven(user_val))
-        std::cout << "This number is Even" << std::endl;
-    else
-        std::cout << "This number is Odd" << std::endl;
+        std::cout << std::boolalpha;
 
+        std::cout << b1 << std::endl;
+        std::cout << !b1 << std::endl;
+
+        std::cout << "(true || true) : " << (true || true) << std::endl; 
+        std::cout << "(true && false) : " << (true && false) << std::endl; 
+        std::cout << "(true || false) : " << (true || false) << std::endl;
+        std::cout << "(false && false) : " << (false && false) << std::endl;
+    }
+
+    {
+        if (true)
+            std::cout << "This is true" << std::endl;
+        else
+            std::cout << "This is false" << std::endl;        
+    }
+
+    {
+        std::cout <<  "isEqual : " << isEqual(1, 2) << std::endl;
+    }
+
+    {
+        bool user_input;
+
+        std::cout << "type your boolean value : ";
+        std::cin >> user_input;
+        std::cout << "user_input : " << user_input << std::endl;
+    }
+
+    {
+        std::cout << "Type any Number : ";
+        std::cin >> user_val;
+
+        if (isEven(user_val))
+            std::cout << "This number is Even" << std::endl;
+        else
+            std::cout << "This number is Odd" << std::endl;
+    }
+
+    
     return 0;
 }
