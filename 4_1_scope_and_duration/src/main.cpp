3766eabac4b58f698fe0e343c74bc00f447dedaa
kimsooyoung <tge1375@naver.com>
2020-11-14 11:30:50 +0900

    4_1 Update

--- 4_1_scope_and_duration/src/main.cpp
+++ 4_1_scope_and_duration/src/main.cpp
@@ -1,19 +1,74 @@
 #include <iostream>
 
+namespace work1 {
+    int a = 1;
+    void doSomething(){
+        a += 3;
+    }
+}
+
+
+namespace work2{
+    int a = 1;
+    void doSomething(){
+        a += 5;
+    }
+}
+
+// nested namespace : from C++17
+// namespace space1{
+//     namespace space2{
+//         namespace space3{
+//             int a = 1;
+//             void doSomething(){
+//                 a += 3;
+//             }
+//         }
+//     }
+// }
+
+namespace space1::space2::space3{
+    int a = 1;
+    void doSomething(){
+        a += 3;
+    }
+}
+
 int main()
 {
+
     {
-        bool item1_flag = false;
-        bool item2_flag = false;
-        bool item3_flag = false;
-        bool item4_flag = false;
+        int apple = 5;
 
-        // event
-        item1_flag = true;
+        std::cout << "apple : " << apple << std::endl;
+        
+        {
+            apple = 1;
+            std::cout << "apple : " << apple << std::endl;
+        }
 
-        // die! item2 loss
-        item2_flag = false;
+        std::cout << "apple : " << apple << std::endl;
     }
 
+    {
+        int apple = 5;
+
+        std::cout << "apple : " << apple << std::endl;
+
+        {
+            int apple2 = 1;
+            std::cout << "apple : " << apple << std::endl;
+        }
+        std::cout << "apple : " << apple << std::endl;
+    }
+
+    {
+        work1::a;
+        work1::doSomething();
+
+        work2::a;
+        work2::doSomething();
+    }
+    
     return 0;
 }
