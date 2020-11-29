39d2fbb6ed69360a5a02c3114f85b30c21fec9a1
swimming_kim <tge1375@naver.com>
2020-11-29 20:56:43 +0900

    Upto 5_4
    Signed-off-by: swimming_kim <tge1375@naver.com>

---
+++ 5_4_goto/src/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+
+    {
+        using namespace std;
+
+        double x;
+
+    Again:
+        cout << "type any number : ";
+        cin >> x;
+
+        if (x < 0.0)
+            goto Again;
+
+        cout << "Sqrt : " << sqrt(x) << endl;
+    }
+
+    {
+        // Error example
+        goto skip;
+        int x;
+
+    skip:
+        x += 3;
+        return 0;
+    }
+
+    return 0;
+}
