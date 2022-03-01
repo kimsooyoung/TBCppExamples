c4bafce9d47d9c64bd31117758d87f866676321c
kimsooyoung <tge1375@naver.com>
2022-03-01 16:34:46 +0900

    Recap

--- 19_1_lambda_function_std_function_bind_foreach/src/main.cpp
+++ 19_1_lambda_function_std_function_bind_foreach/src/main.cpp
@@ -34,6 +34,12 @@ int main(){
     {
         string name = "Mrs. Puff";
         [&]() -> void { cout << name << endl; } ();
+        // reference for specific variable
+        [&name]() -> void { cout << name << endl; } ();
+        // copy
+        [=]() -> void { cout << name << endl; } ();
+        // for Class
+        // [this]() -> void { cout << name << endl; } ();
     }
 
     // for_each with lambda
@@ -59,8 +65,23 @@ int main(){
         std::function<void(int)> func3 = func2;
         func3(123);
 
+        // functoinal with two arguments
+        auto two_arg_func = [&](const string &name, const int &age) -> void { 
+            std::cout << "name : " << name <<
+                " age : " << age << std::endl;
+        };
+        std::function<void(string, int)> two_arg_functor = two_arg_func;
+        two_arg_func("Kim", 27);
+
         std::function<void()> func4 = std::bind(func3, 456);
+        std::function<void()> func5 = std::bind(func2, 789);
+        // std::function with std::bind
+        std::function<void(int)> func6 = std::bind(func2, std::placeholders::_1);
+
+
         func4();
+        func5();
+        func6(100100);
     }
 
     // std::bind and placeholder
@@ -69,6 +90,8 @@ int main(){
 
         auto hello_bind = std::bind(&Object::hello, &instance, std::placeholders::_1);
         hello_bind(string("Workout"));
+
+        std::cout << typeid(hello_bind).name() << std::endl;
         
         auto goodbye_bind = std::bind(&goodbye, std::placeholders::_1);
         goodbye_bind(string("Workout Workout"));
