56f235d0aa20a3fdd637b4f6df2651e5f9f94a3f
kimsooyoung <tge1375@naver.com>
2022-04-13 01:26:34 +0900

    Reformatted in MAC
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- 9_1_arithmetic_operator_overloading/src/main.cpp
+++ 9_1_arithmetic_operator_overloading/src/main.cpp
@@ -2,36 +2,42 @@
 
 using namespace std;
 
-class Cents{
+class Cents
+{
 private:
     int m_cents;
+
 public:
-    Cents(const int &cent_in=0): m_cents(cent_in) {}
+    Cents(const int &cent_in = 0) : m_cents(cent_in) {}
     int getCents() const { return m_cents; }
 
-    friend Cents operator + (const Cents &c1, const Cents & c2);
+    friend Cents operator+(const Cents &c1, const Cents &c2);
 
-    Cents operator + (const Cents & c){
+    Cents operator+(const Cents &c)
+    {
         // return Cents( this->getCents() + c.m_cents );
-        return Cents( this->m_cents + c.m_cents );
+        return Cents(this->m_cents + c.m_cents);
     }
 };
 
-Cents addCents(const Cents &c1, const Cents & c2){
-    return Cents( c1.getCents() + c2.getCents() );
+Cents addCents(const Cents &c1, const Cents &c2)
+{
+    return Cents(c1.getCents() + c2.getCents());
 }
 
 // operator overloading
-Cents operator + (const Cents &c1, const Cents & c2){
+Cents operator+(const Cents &c1, const Cents &c2)
+{
     // return Cents( c1.getCents() + c2.getCents() );
     return Cents(c1.m_cents + c2.m_cents);
 }
 
-int main(){
-    
+int main()
+{
+
     // Add Cents - without operator
     {
-        cout << addCents( Cents(1), Cents(2) ).getCents() << endl;
+        cout << addCents(Cents(1), Cents(2)).getCents() << endl;
     }
 
     // Add Cents - with operator
