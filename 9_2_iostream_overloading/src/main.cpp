b81d29a35d2e780748398365cd8cacf10d20e1af
kimsooyoung <tge1375@naver.com>
2022-04-13 01:26:48 +0900

    Feature : ifstream with cin example
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- 9_2_iostream_overloading/src/main.cpp
+++ 9_2_iostream_overloading/src/main.cpp
@@ -4,36 +4,46 @@
 
 using namespace std;
 
-class Point {
+class Point
+{
 private:
     double m_x, m_y, m_z;
+
 public:
-    Point(const double& x_in= 0.0, const double& y_in= 0.0, const double& z_in= 0.0) : 
-        m_x(x_in), m_y(y_in), m_z(z_in) { }
-    void setPoint(const double& x_in, const double& y_in, const double& z_in){
+    Point(const double &x_in = 0.0, const double &y_in = 0.0, const double &z_in = 0.0) : m_x(x_in), m_y(y_in), m_z(z_in) {}
+
+    void setPoint(const double &x_in, const double &y_in, const double &z_in)
+    {
         m_x = x_in;
         m_y = y_in;
         m_z = z_in;
     }
-    std::tuple<double, double, double> getPoint() const {
+
+    std::tuple<double, double, double> getPoint() const
+    {
         return std::make_tuple(m_x, m_y, m_z);
     }
+
     // opeartor << overloading requires specific format
-    friend std::ostream& operator<<(std::ostream& out, const Point &p_in){
+    friend std::ostream &operator<<(std::ostream &out, const Point &p_in)
+    {
         out << "(" << p_in.m_x << "," << p_in.m_y << "," << p_in.m_z << ")";
         return out;
     }
-    friend std::istream& operator>>(std::istream& in, Point &p_in){
+
+    friend std::istream &operator>>(std::istream &in, Point &p_in)
+    {
         in >> p_in.m_x >> p_in.m_y >> p_in.m_z;
         return in;
     }
 };
 
-int main(){
+int main()
+{
 
     // Basic Usage
     {
-        Point p_1 ( 1.1, 2.2, 3.3 );
+        Point p_1(1.1, 2.2, 3.3);
         Point p_2;
 
         cout << p_1 << endl;
@@ -46,11 +56,11 @@ int main(){
     {
         ofstream of("output.txt");
 
-        Point p_1 ( 1.1, 2.2, 3.3 );
-        Point p_2 ( 4.4, 5.5, 6.6 );
+        Point p_1(1.1, 2.2, 3.3);
+        Point p_2(4.4, 5.5, 6.6);
 
         cout << p_1 << p_2 << endl;
-        of   << p_1 << p_2 << endl;
+        of << p_1 << p_2 << endl;
 
         of.close();
 
@@ -58,9 +68,26 @@ int main(){
         // (1.1,2.2,3.3)(4.4,5.5,6.6)
     }
 
-    // TODO: complete ifstream example
     {
-        // ifstream if
+        Point p_1;
+
+        while (true)
+        {
+            cout << "Type Three coordinate elements (x y z) : ";
+            cin >> p_1;
+
+            if (cin.fail())
+            {
+                cout << "Invalid number!" << endl;
+                cin.clear();
+                cin.ignore(32767, '\n');
+            }
+            else
+            {
+                cout << "Your Result : " << p_1 << endl;
+                cin.ignore(32767, '\n');
+            }
+        }
     }
 
     return 0;
