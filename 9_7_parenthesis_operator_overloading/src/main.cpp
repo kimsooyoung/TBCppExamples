36a39e2378022b031ed86aca572597755f79bb94
kimsooyoung <tge1375@naver.com>
2022-04-13 21:39:27 +0900

    Recap
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- 9_7_parenthesis_operator_overloading/src/main.cpp
+++ 9_7_parenthesis_operator_overloading/src/main.cpp
@@ -2,26 +2,43 @@
 
 using namespace std;
 
-class Accumulator{
+class Accumulator {
 private:
-    int m_counter = 0;
+  int m_counter = 0;
+
 public:
-    Accumulator(){}
-    int operator() (int i){
-        m_counter += i;
-        return m_counter;
-    }
+  Accumulator() {}
+  int operator()(int i) {
+    m_counter += i;
+    return m_counter;
+  }
 };
 
-int main(){
+class Deccel {
+private:
+  int number_;
 
-    // () operator overloaded
-    // it is used like function
-    // So it's also referred to Functor.
-    Accumulator acc;
+public:
+  Deccel(const int &number = 0) : number_(number) {}
 
-    cout << acc(10) << endl;
-    cout << acc(20) << endl;
+  int operator()(int i) {
+    number_ = number_ - i;
+    return number_;
+  }
+};
 
-    return 0;
+int main() {
+
+  // () operator overloaded
+  // it is used like function
+  // So it's also referred to Functor.
+  Accumulator acc;
+
+  cout << acc(10) << endl;
+  cout << acc(20) << endl;
+
+  Deccel dec;
+  cout << dec(10) << endl;
+
+  return 0;
 }
--- 9_8_typecast_overloading/src/main.cpp
+++ 9_8_typecast_overloading/src/main.cpp
@@ -2,59 +2,63 @@
 
 using namespace std;
 
-class Cents{
+class Cents {
 private:
-    int m_val;
+  int m_val;
+
 public:
-    Cents(const int &val_in = 0): m_val(val_in) {}
-    int getCents() const {
-        return m_val;
-    }
-    void setCents(const int &val_in){
-        m_val = val_in;
-    }
+  Cents(const int &val_in = 0) : m_val(val_in) {}
+  int getCents() const { return m_val; }
+  void setCents(const int &val_in) { m_val = val_in; }
 
-    friend std::ostream& operator<<(std::ostream& out, const Cents &c_in){
-        out << c_in.m_val;
-        return out;
-    }
+  friend std::ostream &operator<<(std::ostream &out, const Cents &c_in) {
+    out << c_in.m_val;
+    return out;
+  }
 
-    // casting operator overloaded
-    operator int(){
-        cout << "cast here" << endl;
-        return m_val;
-    }
+  // casting operator overloaded
+  operator int() {
+    cout << "cast here" << endl;
+    return m_val;
+  }
 };
 
 class Dollar {
 private:
-    int m_val;
+  int m_val;
+
 public:
-    Dollar(const int &val_in): m_val(val_in){}
-    operator Cents(){
-        return Cents(m_val * 100);
-    }
+  Dollar(const int &val_in) : m_val(val_in) {}
+  operator Cents() { return Cents(m_val * 100); }
 };
 
-int main(){
+void parseInt(const int &i) { cout << "Paresed Int : " << i << endl; }
 
-    {
-        Cents c1;
-        c1.setCents(112);
-        
-        // we implemented all these castings!!
-        int a = (int)c1;
-        int b = int(c1);
-        int c = static_cast<int>(c1);
+int main() {
 
-        cout << c1 << " " << a << " " << b << " " << c << endl;
-    }
+  {
+    Cents c(10);
 
-    {
-        Dollar d(12);
-        Cents c = d;
-        
-        cout << "Dollar => Cents Casting : " << c << endl;
-    }
-    return 0;
+    parseInt(c);
+  }
+
+  {
+    Cents c1;
+    c1.setCents(112);
+
+    // we implemented all these castings!!
+    int a = (int)c1;
+    int b = int(c1);
+    int c = static_cast<int>(c1);
+
+    cout << c1 << " " << a << " " << b << " " << c << endl;
+  }
+
+  {
+    Dollar d(12);
+    Cents c = d;
+
+    cout << "Dollar => Cents Casting : " << c << endl;
+  }
+  return 0;
 }
