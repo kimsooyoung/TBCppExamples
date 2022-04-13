189612b00de97883ec33a6ffc1866ea4a5848ac4
kimsooyoung <tge1375@naver.com>
2022-04-13 21:38:11 +0900

    Format on MAC
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- 5_10_cin/src/main.cpp
+++ 5_10_cin/src/main.cpp
@@ -1,76 +1,74 @@
-#include <iostream> 
+#include <iostream>
 
-int getInt(){
-    int input;
+int getInt() {
+  int input;
 
-    while(1){
-        std::cout << "Enter Int Number : ";
-        std::cin >> input;
+  while (1) {
+    std::cout << "Enter Int Number : ";
+    std::cin >> input;
 
-        if(std::cin.fail()){
-            std::cin.clear();
-            std::cin.ignore(32767, '\n');
-            std::cout << "Invalid Number, Try again" << std::endl;
-        }
-        else{
-            std::cin.ignore(32767, '\n');
-            return input;
-        }
+    if (std::cin.fail()) {
+      std::cin.clear();
+      std::cin.ignore(32767, '\n');
+      std::cout << "Invalid Number, Try again" << std::endl;
+    } else {
+      std::cin.ignore(32767, '\n');
+      return input;
     }
+  }
 }
 
-char getOperator(){
-    char op;
+char getOperator() {
+  char op;
 
-    while(1){
-        std::cout << "Enter an Operator (+,-,*,/) : " ;
-        std::cin >> op;
+  while (1) {
+    std::cout << "Enter an Operator (+,-,*,/) : ";
+    std::cin >> op;
 
-        std::cin.ignore(32767, '\n');
-        
-        if( op == '+' || op == '-' || op == '*' || op == '/' ){
-            return op;
-        } else {
-            std::cout << "Invalid Operator!!" << std::endl;
-        }
+    std::cin.ignore(32767, '\n');
+
+    if (op == '+' || op == '-' || op == '*' || op == '/') {
+      return op;
+    } else {
+      std::cout << "Invalid Operator!!" << std::endl;
     }
+  }
 
-    return op;
+  return op;
 }
 
-int printResult(int x, int y, char op){
-    std::cout << "result : ";
+int printResult(int x, int y, char op) {
+  std::cout << "result : ";
 
-    switch (op)
-    {
-    case '+':
-        std::cout << x + y << std::endl;
-        break;
-    case '-':
-        std::cout << x - y << std::endl;
-        break;
-    case '*':
-        std::cout << x * y << std::endl;
-        break;
-    case '/':
-        std::cout << x / y << std::endl;
-        break;
-    default:
-        break;
-    }
+  switch (op) {
+  case '+':
+    std::cout << x + y << std::endl;
+    break;
+  case '-':
+    std::cout << x - y << std::endl;
+    break;
+  case '*':
+    std::cout << x * y << std::endl;
+    break;
+  case '/':
+    std::cout << x / y << std::endl;
+    break;
+  default:
+    break;
+  }
 }
 
 int main() {
 
-    while(1){
-        std::cout << "======= Calculator Start ======= " << std::endl;
-        std::cout << "type ctrl + c to Exit" << std::endl;
-        int x = getInt();
-        char ch = getOperator();
-        int y = getInt();
+  while (1) {
+    std::cout << "======= Calculator Start ======= " << std::endl;
+    std::cout << "type ctrl + c to Exit" << std::endl;
+    int x = getInt();
+    char ch = getOperator();
+    int y = getInt();
 
-        printResult(x, y, ch);
-    }
+    printResult(x, y, ch);
+  }
 
-    return 0;
+  return 0;
 }
