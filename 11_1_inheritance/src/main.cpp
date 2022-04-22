170ebb769ea18952ae89e5802a0782e03a99915b
kimsooyoung <tge1375@naver.com>
2022-04-22 15:40:45 +0900

    Update : person -> student / teacher inheritance
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- 11_1_inheritance/src/main.cpp
+++ 11_1_inheritance/src/main.cpp
@@ -2,51 +2,48 @@
 
 using namespace std;
 
-class Mother{
+class Mother {
 private:
-    int m_i;
+  int m_i;
+
 public:
-    Mother(const int &i_in=0): m_i(i_in) {}
+  Mother(const int &i_in = 0) : m_i(i_in) {}
 
-    void setVal(const int &i_in){
-        m_i = i_in;
-    }
+  void setVal(const int &i_in) { m_i = i_in; }
 
-    int getVal() const {
-        return m_i;
-    }
+  int getVal() const { return m_i; }
 };
 
 class Child : public Mother {
 private:
-    double m_d;
+  double m_d;
+
 public:
-    Child(const int &i_in=0, const double &d_in=0.0): m_d(d_in), Mother(i_in) {}
+  Child(const int &i_in = 0, const double &d_in = 0.0)
+      : m_d(d_in), Mother(i_in) {}
 
-    void setVal(const int &i_in, const double &d_in){
-        Mother::setVal(i_in);
-        m_d = d_in;
-    }
+  void setVal(const int &i_in, const double &d_in) {
+    Mother::setVal(i_in);
+    m_d = d_in;
+  }
 
-    double getVal() const {
-        return m_d;
-    }
+  double getVal() const { return m_d; }
 };
 
-int main(){
-    // Child can use any methods in Mother
-    {
-        Child c;
-        c.setVal(113, 1.5707);
-        std::cout << c.getVal() << std::endl;
-    }
+int main() {
+  // Child can use any methods in Mother
+  {
+    Child c;
+    c.setVal(113, 1.5707);
+    std::cout << c.getVal() << std::endl;
+  }
 
-    // setVal in Child has more high priority
-    {
-        Child c;
-        c.setVal(113, 1.5707);
-        std::cout << c.getVal() << std::endl;
-    }
+  // setVal in Child has more high priority
+  {
+    Child c;
+    c.setVal(113, 1.5707);
+    std::cout << c.getVal() << std::endl;
+  }
 
-    return 0;
+  return 0;
 }
--- 11_2_inheritance_2/src/Person.h
+++ 11_2_inheritance_2/src/Person.h
@@ -5,20 +5,17 @@
 
 class Person {
 private:
-    std::string m_name;
+  std::string m_name;
+
 public:
-    Person(const std::string &name_in="No Name"): m_name(name_in) {}
-    
-    void setName(const std::string &name_in){
-        m_name = name_in;
-    }
+  Person(const std::string &name_in = "No Name") : m_name(name_in) {}
 
-    std::string getName() const {
-        return m_name;
-    }
+  void setName(const std::string &name_in) { m_name = name_in; }
 
-    friend std::ostream& operator<<(std::ostream& out, const Person &p_in){
-        out << " name is : " << p_in.m_name;
-        return out;
-    }
+  std::string getName() const { return m_name; }
+
+  friend std::ostream &operator<<(std::ostream &out, const Person &p_in) {
+    out << " name is : " << p_in.m_name;
+    return out;
+  }
 };
