fa52fea0e3fd48a6e7efb1e8d7a89e530d8a9210
kimsooyoung <tge1375@naver.com>
2023-04-22 10:24:05 +0900

    prettier code
    Signed-off-by: kimsooyoung <tge1375@naver.com>

--- 19_1_lambda_function_std_function_bind_foreach/src/main.cpp
+++ 19_1_lambda_function_std_function_bind_foreach/src/main.cpp
@@ -1,101 +1,97 @@
+#include <algorithm>
+#include <functional>
 #include <iostream>
 #include <string>
-#include <algorithm>
 #include <vector>
-#include <functional>
 
 using namespace std;
 
 class Object {
 public:
-    void hello(const string &greet_msg){
-        cout << greet_msg << endl;
-    }
+  void hello(const string &greet_msg) { cout << greet_msg << endl; }
 };
 
-void goodbye(const string &bye_msg){
-    cout << bye_msg << endl;
-}
+void goodbye(const string &bye_msg) { cout << bye_msg << endl; }
 
-int main(){
+int main() {
 
-    // Modern C++, Lambda function
+  // Modern C++, Lambda function
+  {
+    auto printFunc = [](const string &str_in) -> void {
+      cout << str_in << endl;
+    };
+
+    printFunc("Yellow Horld");
+
+    [](const int &i) -> void { cout << i * 111111 << endl; }(4);
+  }
+
+  // lambda function with reference
+  {
+    string name = "Mrs. Puff";
+    [&]() -> void { cout << name << endl; }();
+    // reference for specific variable
+    [&name]() -> void { cout << name << endl; }();
+    // copy
+    [=]() -> void { cout << name << endl; }();
+    // for Class
+    // [this]() -> void { cout << name << endl; } ();
+  }
+
+  // for_each with lambda
+  {
+    vector<int> vec = {1, 2, 3, 4, 5};
+
+    // auto printFunc = [](const int &val){ cout << val << endl; };
+    // for_each(vec.begin(), vec.end(), printFunc);
+
+    // same with above but more prefered.
+    for_each(vec.begin(), vec.end(),
+             [](const int &val) { cout << val << " "; });
+    cout << endl;
+
+    // lambda function - return
     {
-        auto printFunc = [](const string &str_in) -> void {
-            cout << str_in << endl;
-        };
-
-        printFunc("Yellow Horld");
-
-        [](const int &i) -> void { cout << i * 111111 << endl; }(4);
+      cout << []() -> int { return 1; }() << endl;
     }
+  }
 
-    // lambda function with reference
-    {
-        string name = "Mrs. Puff";
-        [&]() -> void { cout << name << endl; } ();
-        // reference for specific variable
-        [&name]() -> void { cout << name << endl; } ();
-        // copy
-        [=]() -> void { cout << name << endl; } ();
-        // for Class
-        // [this]() -> void { cout << name << endl; } ();
-    }
+  // functional and bind
+  {
+    auto func2 = [](const int &val) -> void { cout << val << endl; };
+    std::function<void(int)> func3 = func2;
+    func3(123);
 
-    // for_each with lambda
-    {
-        vector<int> vec = {1,2,3,4,5}; 
+    // functoinal with two arguments
+    auto two_arg_func = [&](const string &name, const int &age) -> void {
+      std::cout << "name : " << name << " age : " << age << std::endl;
+    };
+    std::function<void(string, int)> two_arg_functor = two_arg_func;
+    two_arg_func("Kim", 27);
 
-        // auto printFunc = [](const int &val){ cout << val << endl; };
-        // for_each(vec.begin(), vec.end(), printFunc);
-        
-        // same with above but more prefered.
-        for_each(vec.begin(), vec.end(), [](const int &val){ cout << val << " ";} );
-        cout << endl;
+    std::function<void()> func4 = std::bind(func3, 456);
+    std::function<void()> func5 = std::bind(func2, 789);
+    // std::function with std::bind
+    std::function<void(int)> func6 = std::bind(func2, std::placeholders::_1);
 
-        // lambda function - return 
-        {
-            cout << []() -> int { return 1; } () << endl;
-        }
-    }
+    func4();
+    func5();
+    func6(100100);
+  }
 
-    // functional and bind
-    {
-        auto func2 = [](const int &val) -> void{ cout << val << endl; };
-        std::function<void(int)> func3 = func2;
-        func3(123);
+  // std::bind and placeholder
+  {
+    Object instance;
 
-        // functoinal with two arguments
-        auto two_arg_func = [&](const string &name, const int &age) -> void { 
-            std::cout << "name : " << name <<
-                " age : " << age << std::endl;
-        };
-        std::function<void(string, int)> two_arg_functor = two_arg_func;
-        two_arg_func("Kim", 27);
+    auto hello_bind =
+        std::bind(&Object::hello, &instance, std::placeholders::_1);
+    hello_bind(string("Workout"));
 
-        std::function<void()> func4 = std::bind(func3, 456);
-        std::function<void()> func5 = std::bind(func2, 789);
-        // std::function with std::bind
-        std::function<void(int)> func6 = std::bind(func2, std::placeholders::_1);
+    std::cout << typeid(hello_bind).name() << std::endl;
 
+    auto goodbye_bind = std::bind(&goodbye, std::placeholders::_1);
+    goodbye_bind(string("Workout Workout"));
+  }
 
-        func4();
-        func5();
-        func6(100100);
-    }
-
-    // std::bind and placeholder
-    {
-        Object instance;
-
-        auto hello_bind = std::bind(&Object::hello, &instance, std::placeholders::_1);
-        hello_bind(string("Workout"));
-
-        std::cout << typeid(hello_bind).name() << std::endl;
-        
-        auto goodbye_bind = std::bind(&goodbye, std::placeholders::_1);
-        goodbye_bind(string("Workout Workout"));
-    }
-
-    return 0;
+  return 0;
 }
--- 19_5_task_based_async_programming/src/main.cpp
+++ 19_5_task_based_async_programming/src/main.cpp
@@ -4,81 +4,73 @@
 
 using namespace std;
 
-int main(){
+int main() {
 
-    // multi threading
-    {
-        int result;
-        thread t1 = thread([&](){
-            result = 1 + 2;
-        });
+  // multi threading
+  {
+    int result;
+    thread t1 = thread([&]() { result = 1 + 2; });
 
-        t1.join();
-        cout << result << endl;
-    }
+    t1.join();
+    cout << result << endl;
+  }
 
-    // task-based parallelism
-    {
-        auto fut = async([](){
-            return 1 + 2;
-        });
+  // task-based parallelism
+  {
+    auto fut = async([]() { return 1 + 2; });
 
-        cout << fut.get() << endl;
-    }
+    cout << fut.get() << endl;
+  }
 
+  // future with promise
+  {
+    std::promise<int> prom;
+    auto fut = prom.get_future();
 
-    // future with promise
-    {
-        std::promise<int> prom;
-        auto fut = prom.get_future();
+    thread t1 =
+        thread([](promise<int> &&prom) { prom.set_value(1 + 2); }, move(prom));
 
-        thread t1 = thread([](promise<int> &&prom){
-            prom.set_value(1 + 2);
-        }, move(prom));
+    cout << fut.get() << endl;
+    t1.join();
+  }
 
-        cout << fut.get() << endl;
-        t1.join();
-    }
+  // async vs thread
 
-    // async vs thread
+  // async
+  {
+    auto fut1 = async([]() {
+      cout << "[async] First work started" << endl;
+      std::this_thread::sleep_for(std::chrono::seconds(2));
+      cout << "[async] First work done.. " << endl;
+    });
 
-    // async
-    {
-        auto fut1 = async([](){
-            cout << "[async] First work started" << endl; 
-            std::this_thread::sleep_for(std::chrono::seconds(2));
-            cout << "[async] First work done.. " << endl;
-        });
+    auto fut2 = async([]() {
+      cout << "[async] Second work started" << endl;
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+      cout << "[async] Second work done.. " << endl;
+    });
 
-        auto fut2 = async([](){
-            cout << "[async] Second work started" << endl; 
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            cout << "[async] Second work done.. " << endl;
-        });
+    fut1.get();
+    fut2.get();
+  }
 
-        fut1.get();
-        fut2.get();
-    }
+  // same work with thread
+  {
+    auto t1 = thread([]() {
+      cout << "[thread] First work started" << endl;
+      std::this_thread::sleep_for(std::chrono::seconds(2));
+      cout << "[thread] First work done.. " << endl;
+    });
 
-    // same work with thread
-    {
-        auto t1 = thread([](){
-            cout << "[thread] First work started" << endl; 
-            std::this_thread::sleep_for(std::chrono::seconds(2));
-            cout << "[thread] First work done.. " << endl;
-        });
+    auto t2 = thread([]() {
+      cout << "[thread] Second work started" << endl;
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+      cout << "[thread] Second work done.. " << endl;
+    });
 
-        auto t2 = thread([](){
-            cout << "[thread] Second work started" << endl; 
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            cout << "[thread] Second work done.. " << endl;
-        });
+    t1.join();
+    t2.join();
+  }
 
-        t1.join();
-        t2.join();
-    }
-
-
-    return 0;
-
+  return 0;
 }
